559f0cf62d717e39c6bd6976efc23aafca320a4b
richard200 <richmn200@gmail.com>
2022-12-06 14:59:17 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let listen = document.querySelector('form')
+  listen.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTasks(e.target.task.value);
+    
+
+  })
+
+   
+})
+
+function handleTasks(task){
+  let p = document.createElement('p')
+  p.textContent = task;
+  document.querySelector('#list').appendChild(p)
+
+}
